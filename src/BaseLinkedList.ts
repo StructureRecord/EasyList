17fb6e6cfa9cf830fc7b1c28e7c4416b3e8de863
Guest User <Guest@MITR-C07TM33MG1HW.local>
2020-07-27 13:22:27 +0530

    initial code

---
+++ src/BaseLinkedList.ts
@@ -0,0 +1,75 @@
+export default class BaseLinkedList {
+  headNode: any;
+  constructor(headNode = null) {
+    this.headNode = headNode;
+  }
+
+  get firstNode() {
+    return this.headNode;
+  }
+
+  get lastNode() {
+    if (!this.listSize) {
+      return;
+    }
+    return this.getNodeElement(this.listSize - 1);
+  }
+
+  get listSize() {
+    let currentHead = this.headNode;
+    let count: number = 0;
+    while (currentHead) {
+      currentHead = currentHead.next;
+      count++;
+    }
+    return count;
+  }
+
+  checkIndex(index: number) {
+    try {
+      if (index < 0) {
+        throw "Index should not be less than 0";
+      }
+      if (index > this.listSize) {
+        throw "Index value is greater than linked list list size";
+      }
+    } catch (err) {
+      console.log(err);
+      return true;
+    }
+  }
+
+  clearNodeList() {
+    this.headNode = null;
+  }
+
+  getNodeElement(index: number) {
+    this.checkIndex(index);
+    let currentHead = this.headNode;
+    let count: number = 0;
+    while (currentHead) {
+      if (count === index) {
+        return currentHead;
+      }
+      count++;
+      currentHead = currentHead.next;
+    }
+    return null;
+  }
+
+  insertNode = (data: any, index: number) => {}
+
+  deleteNode = (index: any) => {}
+
+  insertNodeAtStart(data: any) {}
+
+  insertNodeAtEnd(data: any) {}
+
+  insertNodeAtIndex(data: any, index: number) {}
+
+  deleteFirstNode() {}
+
+  deleteLastNode() {}
+
+  deleteNodeAtIndex(index: number) {}
+}
